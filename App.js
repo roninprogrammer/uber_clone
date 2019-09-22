9c0897cdd2a8e18006a57ec35aa1f25f6404564a
vicknesh balasubramaniam <roninprogrammer@gmail.com>
2019-09-22 20:41:18 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Platform , StatusBar, StyleSheet, View } from 'react-native'
+import firebase from 'firebase'
+import AppNavigator from './navigation/AppNavigator'
+
